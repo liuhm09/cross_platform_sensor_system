7b19200ec1880b5ad52f6ef2493d4e9d4c1ffad3
Tomorrow <liuhm09@gmail.com>
2014-04-15 21:14:22 +0800

    sensors monitor completed

--- android/src/com/geekerchina/feeltheworld/MainActivity.java
+++ android/src/com/geekerchina/feeltheworld/MainActivity.java
@@ -1,127 +1,31 @@
 package com.geekerchina.feeltheworld;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.Certificate;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.List;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.geekerchina.feeltheworld.MainActivity;
 import com.geekerchina.feeltheworld.R;
-
-import android.net.wifi.WifiConfiguration.Protocol;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.telephony.SmsMessage;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Button;
 
 
 
 
 
-public class MainActivity extends Activity implements SensorEventListener {
-
-	private SensorManager sMgr;
-	private Sensor mAccelerometer;
-	private boolean mInitialized; 
-	private final float NOISE = (float) 2.0;
-	private float mLastX, mLastY, mLastZ;
+public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mInitialized = false;
-		sMgr = (SensorManager)this.getSystemService(SENSOR_SERVICE);
-		mAccelerometer = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		sMgr.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-		
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
 	}
 
 
-	   @Override
-	   public boolean onCreateOptionsMenu(Menu menu) {
-	      // Inflate the menu; this adds items to the action bar if it is present.
-	      getMenuInflater().inflate(R.menu.activity_main, menu);
-	      return true;
-	   }
-
-	
-	   protected void onResume() {
-		   super.onResume();
-		   sMgr.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-		   }
-		   protected void onPause() {
-		   super.onPause();
-		   sMgr.unregisterListener(this);
-		   }
-
-		   @Override
-			  public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-			    // Do something here if sensor accuracy changes.
-			  }
-		   
-	public static String convertStreamToString(InputStream inputStream) throws IOException {
-        if (inputStream != null) {
-            Writer writer = new StringWriter();
-
-            char[] buffer = new char[1024];
-            try {
-                Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"),1024);
-                int n;
-                while ((n = reader.read(buffer)) != -1) {
-                    writer.write(buffer, 0, n);
-                }
-            } finally {
-                inputStream.close();
-            }
-            return writer.toString();
-        } else {
-            return "";
-        }
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu; this adds items to the action bar if it is present.
+	    getMenuInflater().inflate(R.menu.activity_main, menu);
+	    return true;
+	}
 	public void startShowsensors(View view)
 	{
 		Intent intent = new Intent(this, ShowsensorsActivity.class);
@@ -131,49 +35,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 	{
 		Intent intent = new Intent(this, SensorsMonitorActivity.class);
 		startActivity(intent);
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		// TODO Auto-generated method stub
-		TextView tvX= (TextView)findViewById(R.id.textView2);
-		TextView tvY= (TextView)findViewById(R.id.textView3);
-		TextView tvZ= (TextView)findViewById(R.id.textView4);
-//		ImageView iv = (ImageView)findViewById(R.id.image);
-		float x = event.values[0];
-		float y = event.values[1];
-		float z = event.values[2];
-		if (!mInitialized) {
-		mLastX = x;
-		mLastY = y;
-		mLastZ = z;
-		tvX.setText("0.0");
-		tvY.setText("0.0");
-		tvZ.setText("0.0");
-		mInitialized = true;
-		} else {
-		float deltaX = Math.abs(mLastX - x);
-		float deltaY = Math.abs(mLastY - y);
-		float deltaZ = Math.abs(mLastZ - z);
-		if (deltaX < NOISE) deltaX = (float)0.0;
-		if (deltaY < NOISE) deltaY = (float)0.0;
-		if (deltaZ < NOISE) deltaZ = (float)0.0;
-		mLastX = x;
-		mLastY = y;
-		mLastZ = z;
-		tvX.setText(Float.toString(deltaX));
-		tvY.setText(Float.toString(deltaY));
-		tvZ.setText(Float.toString(deltaZ));
-		//iv.setVisibility(View.VISIBLE);
-		//if (deltaX > deltaY) {
-		//iv.setImageResource(R.drawable.horizontal);
-		//} else if (deltaY > deltaX) {
-		//iv.setImageResource(R.drawable.vertical);
-		//} else {
-		//iv.setVisibility(View.INVISIBLE);
-		//}
-		}
-		
-	}  
-	}
+	}	
+}
 	
--- android/src/com/geekerchina/feeltheworld/SensorsMonitorActivity.java
+++ android/src/com/geekerchina/feeltheworld/SensorsMonitorActivity.java
@@ -3,17 +3,14 @@ package com.geekerchina.feeltheworld;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.widget.TextView;
 
-public class SensorsMonitorActivity extends Activity implements SensorEventListener {
+public class SensorsMonitorActivity extends Activity{
 	  private SensorManager sMgr;
 	  private SensorEventListener sEvl;
 	  private Sensor mPressure;
@@ -31,62 +28,71 @@ public class SensorsMonitorActivity extends Activity implements SensorEventListe
 	    // a particular sensor.
 	    mInitialized = false;
 	    sMgr = (SensorManager)this.getSystemService(SENSOR_SERVICE);
-		mAccelerometer = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		sMgr.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-	  }
+	    sEvl = new SensorEventListener() {
+	        @Override
+	        public void onAccuracyChanged(Sensor arg0, int arg1) {
+	        }
 
-	  @Override
-	  public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-	    // Do something here if sensor accuracy changes.
-	  }
+	        @Override
+	        public void onSensorChanged(SensorEvent event) {
+	            Sensor sensor = event.sensor;
+	            if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) 
+	            {
+	                //TODO: get values
+	            	TextView tvX= (TextView)findViewById(R.id.xaccel);
+	    			TextView tvY= (TextView)findViewById(R.id.yaccel);
+	    			TextView tvZ= (TextView)findViewById(R.id.zaccel);
+//	    			ImageView iv = (ImageView)findViewById(R.id.image);
+	    			float x = event.values[0];
+	    			float y = event.values[1];
+	    			float z = event.values[2];
+	    			if (!mInitialized) 
+	    			{
+		    			mLastX = x;
+		    			mLastY = y;
+		    			mLastZ = z;
+		    			tvX.setText("0.0");
+		    			tvY.setText("0.0");
+		    			tvZ.setText("0.0");
+		    			mInitialized = true;
+	    			} 
+	    			else 
+	    			{
+		    			float deltaX = Math.abs(mLastX - x);
+		    			float deltaY = Math.abs(mLastY - y);
+		    			float deltaZ = Math.abs(mLastZ - z);
+		    			if (deltaX < NOISE) deltaX = (float)0.0;
+		    			if (deltaY < NOISE) deltaY = (float)0.0;
+		    			if (deltaZ < NOISE) deltaZ = (float)0.0;
+		    			mLastX = x;
+		    			mLastY = y;
+		    			mLastZ = z;
+		    			tvX.setText(Float.toString(deltaX));
+		    			tvY.setText(Float.toString(deltaY));
+		    			tvZ.setText(Float.toString(deltaZ));
+	    			}
+	    		}
+	    		else if (sensor.getType() == Sensor.TYPE_TEMPERATURE) 
+	    		{
+	                //TODO: get values
+	            	TextView t = (TextView)findViewById(R.id.temperature_show);
+	            	t.setText("00.0");
+	            	t.setText(Float.toString(event.values[0]));
+	            }
+	    		else if (sensor.getType() == Sensor.TYPE_GRAVITY) 
+	    		{
+	                //TODO: get values
+	            	TextView gravity = (TextView)findViewById(R.id.gravity_show);
+	            	gravity.setText("00.0");
+	            	gravity.setText(Float.toString(event.values[0]));
+	            }
+	        }
+	    };
+	    // mAccelerometer = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		// sMgr.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	    sMgr.registerListener(sEvl, sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+	    sMgr.registerListener(sEvl, sMgr.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_GAME);
 
-	  @Override
-	  public final void onSensorChanged(SensorEvent event) {
-		// TODO Auto-generated method stub
-			TextView tvX= (TextView)findViewById(R.id.xaccel);
-			TextView tvY= (TextView)findViewById(R.id.yaccel);
-			TextView tvZ= (TextView)findViewById(R.id.zaccel);
-//			ImageView iv = (ImageView)findViewById(R.id.image);
-			float x = event.values[0];
-			float y = event.values[1];
-			float z = event.values[2];
-			if (!mInitialized) {
-			mLastX = x;
-			mLastY = y;
-			mLastZ = z;
-			tvX.setText("0.0");
-			tvY.setText("0.0");
-			tvZ.setText("0.0");
-			mInitialized = true;
-			} else {
-			float deltaX = Math.abs(mLastX - x);
-			float deltaY = Math.abs(mLastY - y);
-			float deltaZ = Math.abs(mLastZ - z);
-			if (deltaX < NOISE) deltaX = (float)0.0;
-			if (deltaY < NOISE) deltaY = (float)0.0;
-			if (deltaZ < NOISE) deltaZ = (float)0.0;
-			mLastX = x;
-			mLastY = y;
-			mLastZ = z;
-			tvX.setText(Float.toString(deltaX));
-			tvY.setText(Float.toString(deltaY));
-			tvZ.setText(Float.toString(deltaZ));
-			}
-	    // Do something with this sensor data.
-	  }
-
-	  @Override
-	  protected void onResume() {
-	    // Register a listener for the sensor.
-	    super.onResume();
-	    
-	    sMgr.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-	   }
-
-	  @Override
-	  protected void onPause() {
-	    // Be sure to unregister the sensor when the activity pauses.
-	    super.onPause();
-	    sMgr.unregisterListener(this);
+	    sMgr.registerListener(sEvl, sMgr.getDefaultSensor(Sensor.TYPE_TEMPERATURE), SensorManager.SENSOR_DELAY_GAME);
 	  }
 	}
